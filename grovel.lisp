2ed8d3ce568b20ae069c1fffc1a2350b032dfe56
hk <helmut.rohrbacher@gmail.com>
2025-04-12 13:46:27 -0700

    remove old implementations

--- grovel.lisp
+++
@@ -1,5 +0,0 @@
-(in-package #:cl-wasm-runtime)
-
-(include "stddef.h")
-
-(ctype %size-type "size_t")
--- wasm-foreign.lisp
+++
@@ -1,8 +0,0 @@
-(in-package #:cl-wasm-runtime)
-
-(define-wasm-ref foreign)
-
-(cffi:defcfun "wasm_foreign_new" %wasm-foreign-type ; own
-  (store %wasm-store-type))
-
-(define-wasm-object-class foreign)
